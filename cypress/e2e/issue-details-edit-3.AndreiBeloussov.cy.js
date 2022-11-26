579b916d615504a3bafbec7c5a1289884bbfa846
AndreiBeloussov <belderbel@gmail.com>
2022-11-26 11:43:39 +0200

    Changed config

---
+++ cypress/e2e/issue-details-edit-3.AndreiBeloussov.cy.js
@@ -0,0 +1,136 @@
+/**
+ * Workshop #15
+ * 1. Start creating classes for testable objects, such as "Issue"
+ * 2. Move test actions into functions as we did on demo previously
+ *    2.1 Update issue type, description etc.
+ * 3. Use those functions in the spec file
+ *
+ * Expected result:
+ * 1. New class with methods for updating title, description etc.
+ * 2. Variables correctly stored
+ *    Bonus: used random data generator library (faker.js)
+ */
+
+/**
+ * Workshop #16
+ * Task #1
+ * 1. Look for previously created method for validating information in the field (any field)
+ *    1.1 One of them was: cy.get('[data-testid="select:priority"]').should('have.text', 'Medium');
+ * 2. Define an object with expected information in the style: selector/it's value
+ * 3. Update method to go over this object and assert information in the field using "for..of" loop
+ *
+ * Expected result:
+ * 1. You will have defined object with at least priority, status and reporter selectors and their values
+ * 2. Your method runs X amount of times and assert information in the field without code duplication
+ *
+ * Task #2
+ * Most of the field in this file are using should('have.text') assertion, however, there are some which uses should('contain')
+ * From the previous task expand the solution with "if" check which would allow us to assert using different should assertion but still keep all the code inside one loop without creating separated assertion
+ *
+ * Expected result:
+ * 1. Previously created method will have more selectors included in the object (for example, assignees are added)
+ */
+
+import IssueDetailsModal from "../pages/IssueDetailsModal"
+import { faker } from '@faker-js/faker';
+
+describe('Issue details editing', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', 'https://jira.ivorreic.com/project').then((url) => {
+      cy.visit(url + '/board');
+      cy.contains('This is an issue of type: Task.').click();
+    });
+  });
+
+  it('Should update type, status, assignees, reporter, priority successfully', () => {
+    getIssueDetailsModal().within(() => {
+      IssueDetailsModal.issueUpdate();
+      // cy.get(this.issueType).should('contain', 'Story')
+      // cy.get('[data-testid="select:type"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Story"]')
+      //     .trigger('mouseover')
+      //     .trigger('click');
+      // cy.get('[data-testid="select:type"]').should('contain', 'Story');
+
+      // cy.get('[data-testid="select:status"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Done"]').click();
+      // cy.get('[data-testid="select:status"]').should('have.text', 'Done');
+
+      // cy.get('[data-testid="select:assignees"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Lord Gaben"]').click();
+      // cy.get('[data-testid="select:assignees"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      // cy.get('[data-testid="select:assignees"]').should('contain', 'Baby Yoda');
+      // cy.get('[data-testid="select:assignees"]').should('contain', 'Lord Gaben');
+
+      // cy.get('[data-testid="select:reporter"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Pickle Rick"]').click();
+      // cy.get('[data-testid="select:reporter"]').should('have.text', 'Pickle Rick');
+
+      // cy.get('[data-testid="select:priority"]').click('bottomRight');
+      // cy.get('[data-testid="select-option:Medium"]').click();
+      // cy.get('[data-testid="select:priority"]').should('have.text', 'Medium');
+    });
+  });
+
+  it('Should update title, description successfully', () => {
+    const title = faker.word.adjective();
+    const description = faker.lorem.paragraph();
+
+    getIssueDetailsModal().within(() => {
+      IssueDetailsModal.issueTitleDescriptionUpdate();
+      // cy.get('textarea[placeholder="Short summary"]')
+      //   .clear()
+      //   .type(title)
+      //   .blur();
+
+      // cy.get('.ql-snow')
+      //   .click()
+      //   .should('not.exist');
+
+      // cy.get('.ql-editor').clear().type(description);
+
+      // cy.contains('button', 'Save')
+      //   .click()
+      //   .should('not.exist');
+
+      // cy.get('textarea[placeholder="Short summary"]').should('have.text', title);
+      // cy.get('.ql-snow').should('have.text', description);
+    });
+  });
+
+  it('Should delete an issue successfully', () => {
+    getIssueDetailsModal();
+
+    IssueDetailsModal.issueDelete();
+    //   .find(`button ${'[data-testid="icon:trash"]'}`)
+    //   .click();
+
+    // cy.get('[data-testid="modal:confirm"]')
+    //   .contains('button', 'Delete issue')
+    //   .click();
+    // cy.get('[data-testid="modal:confirm"]')
+    //   .should('not.exist');
+
+    // cy.contains('This is an issue of type: Task.').should('not.exist');
+
+  });
+
+
+
+
+  // Workshop 17
+  // Task 3
+  // Add new test to test file, Invoke text value of reporter name and check that it 
+  // has only characters in it (no numbers, no special characters).
+
+
+  it.only('Regex test', () => {
+    getIssueDetailsModal();
+    IssueDetailsModal.RegexReporter();
+
+  });
+
+  const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
+});
